14c2681c5df1a321170e0f9fd34e01e9901f193e
Atulya Bist <atulya2109@gmail.com>
2020-04-08 22:17:47 +0530

    Working on popup view

--- src/extension/popup.js
+++ src/extension/popup.js
@@ -1,15 +1,30 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  var button = document.getElementById('changeColor');
+  // var button = document.getElementById('changeColor');
 
-  button.onclick = function (element) {
+  // button.onclick = function (element) {
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
-      chrome.tabs.sendMessage(tabs[0].id, {type : 'initialize'}, (response) => {
-        console.log(response.status)
-      })
-    })
+  //     chrome.tabs.sendMessage(tabs[0].id, {type : 'initialize'}, (response) => {
+  //       console.log(response.status)
+  //     })
+  //   })
 
+  // };
+
+  var joinServerButton = document.getElementById('joinServerButton')
+
+  var hideMain = function() {
+    var mainDiv = document.getElementsByClassName('main')[0];
+    mainDiv.style.display = "none";
   };
+
+  joinServerButton.addEventListener('click', () => {
+
+    hideMain();
+    var joinServerDiv = document.getElementsByClassName("join-server")[0]
+    joinServerDiv.style.display = "block";
+    
+  })
 })
